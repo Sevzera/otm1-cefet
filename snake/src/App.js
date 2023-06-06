d77133b24eb029380b915876607ce4152d7cc4dd
Sevzera <svzr.47@gmail.com>
2023-06-06 10:59:59 -0300

    colisao

--- snake/src/App.js
+++ snake/src/App.js
@@ -95,10 +95,6 @@ const App = () => {
         const newSnake = [...prevSnake];
         newSnake.unshift(newHead);
 
-        if(snake.some((cell) => cell.x === newHead.x && cell.y === newHead.y)) {
-          console.log('Game Over');
-          clearInterval(moveSnake);}
-
         if (
           foods.some((food) => food.x === newHead.x && food.y === newHead.y)
         ) {
