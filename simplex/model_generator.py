1f367272085cf83b67ed0af9a89a9ec73252835b
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2023-07-03 01:11:57 -0300

    model generator part2

--- simplex/model_generator.py
+++ simplex/model_generator.py
@@ -1,15 +1,18 @@
 from snake import Snake
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--foods", required=True,
+	help="path to model json file")
+args = vars(ap.parse_args())
 
 HIGH_COST = 1000
-FOODS_IN_GAME = 3
+FOODS_IN_GAME = args["foods"]
 
 def main():
     snake = Snake(number_of_foods=FOODS_IN_GAME)
     costs = snake.get_cost_matrix()
     
-    # create json file
-    file = open("model.json", "w")
-    file.write('{\n')
     # recovering all costs between different cities
     C = []
     for routes in costs:
@@ -20,31 +23,28 @@ def main():
     # completing with 2*n artificial variables
     for i in range(2*len(costs)):
         C.append(HIGH_COST)
-    
-    file.write("    \"C\": [\n")
-    for i in range(len(C)):
-        if i == len(C)-1:
-            file.write("        {}\n".format(C[i]))
-        else:
-            file.write("        {},\n".format(C[i]))
-    file.write("    ],\n")
 
+    # finding A, B and N matrices
     A = []
+    B = []
+    N = []
     for variables in range(2*len(costs)):
         # row restrictions
         change_rule = len(costs)
-        row_base = [0]*FOODS_IN_GAME*len(costs)
-        row_non_base = [0]*2*len(costs)
+        non_base_row = [0]*FOODS_IN_GAME*len(costs)
+        base_row = [0]*2*len(costs)
         for food in range(FOODS_IN_GAME):
             if change_rule > variables: 
-                row_base[food + (variables*FOODS_IN_GAME)] = 1
+                non_base_row[food + (variables*FOODS_IN_GAME)] = 1
 
             for row in range(2*len(costs)):
                 if variables == row:
-                    row_non_base[row] = 1
+                    base_row[row] = 1
                 else:
-                    row_non_base[row] = 0
-        A.append(row_base+row_non_base)
+                    base_row[row] = 0
+        N.append(non_base_row)
+        B.append(base_row)
+        A.append(non_base_row+base_row)
 
     matrix = []
 
@@ -74,6 +74,24 @@ def main():
     for r in range(len(costs)):
         for c in range(FOODS_IN_GAME*len(costs)):
             A[r+len(costs)][c] = matrix[r][c]
+            N[r+len(costs)][c] = matrix[r][c]
+
+    # finding b list
+    b = []
+    for i in range(2*len(costs)):
+        b.append(1)
+
+    # save model
+    file = open("model.json", "w")
+    file.write('{\n')
+
+    file.write("    \"C\": [\n")
+    for i in range(len(C)):
+        if i == len(C)-1:
+            file.write("        {}\n".format(C[i]))
+        else:
+            file.write("        {},\n".format(C[i]))
+    file.write("    ],\n")
 
     file.write("    \"A\": [\n")
     for i in range(len(A)):
@@ -82,6 +100,32 @@ def main():
         else:
             file.write("        {},\n".format(A[i]))
     file.write("    ],\n")
-    
+
+    file.write("    \"B\": [\n")
+    for i in range(len(B)):
+        if i == len(B) - 1:
+            file.write("        {}\n".format(B[i]))
+        else:
+            file.write("        {},\n".format(B[i]))
+    file.write("    ],\n")
+
+    file.write("    \"N\": [\n")
+    for i in range(len(N)):
+        if i == len(N) - 1:
+            file.write("        {}\n".format(N[i]))
+        else:
+            file.write("        {},\n".format(N[i]))
+    file.write("    ],\n")
+
+    file.write("    \"b\": [")
+    for i in range(len(b)):
+        if i == len(b) - 1:
+            file.write("{}".format(b[i]))
+        else:
+            file.write("{},".format(b[i]))
+    file.write("]\n}")
+
+    file.close()
+
 if __name__== "__main__":
     main()
